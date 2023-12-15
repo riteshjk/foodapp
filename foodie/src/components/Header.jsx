686d0062e95c2824dc65a2e59113d1c5a29fdd0d
riteshjk <96072567+riteshjk@users.noreply.github.com>
2023-12-15 21:12:09 +0530

    first commit

---
+++ foodie/src/components/Header.jsx
@@ -0,0 +1,17 @@
+const Header =()=>{
+    return(
+        <div className="container">
+        <img className="logo" src="https://i.pinimg.com/originals/75/d8/11/75d8113347755fd2eb070a2cc39d01ba.jpg" alt="" />
+        <div className="sections">
+            <ul>
+                <li>Home</li>
+                <li>About</li>
+                <li>Contact</li>
+                <li>Login</li>
+            </ul>
+        </div>
+        </div>
+    )
+}
+
+export default Header;
